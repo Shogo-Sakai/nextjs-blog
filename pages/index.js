cf0bae78ac8af061e0ee5b3db650d9317dcf0c12
shogosakai <shogosakai73@gmail.com>
2021-05-04 22:53:40 +0900

    cssの調整

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hi, I am a wev developer.]</p>
+        <p>[Hi, I am a web developer. I want to be a fullstack engineer.]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
